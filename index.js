5e4175f4698cb83549bd5dc2d2c055c2b753ba27
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-18 20:05:12 +0600

    adding server side codes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const app = express();
@@ -9,3 +10,13 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vbw8r.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.get('/',async(req,res)=>{
+    res.send('Mobile Bikri Korai server running');
+})
+
+app.listen(por,()=>console.log(`Mobile Bikri Korai running on ${port}`));
