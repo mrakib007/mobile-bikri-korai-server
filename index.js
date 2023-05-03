3d8a8ca7c77b373d85d21536b5e326b1baae91d2
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-05-03 18:24:14 +0600

    adding payment gateway

--- index.js
+++ index.js
@@ -119,6 +119,38 @@ async function run(){
             }
           })
 
+          //payment
+          app.post('/create-payment-intent',async(req,res)=>{
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+              currency: 'bdt',
+              amount: amount,
+              "payment_method_types": [
+                "card"
+              ]
+            });
+            res.send({
+              clientSecret: paymentIntent.client_secret,
+            });
+          })
+
+          app.post('/payments',async(req,res)=>{
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId;
+            const filter = {_id: new ObjectId(id)};
+            const updatedDoc = {
+              $set: {
+                paid: true,
+                transactionId: payment.transactionId,
+              }
+            }
+            const updatedResult = await bookingsCollection.updateOne(filter,updatedDoc);
+            res.send(result);
+          })
+
           app.patch('/myProducts/:id',async(req,res)=>{
             const id = req.params.id;
             const status = req.body.status;
